da8a38adfd08a1215ce7f1c9ce5e1c735d9efabb
kabir325 <kabirsahu725@gmail.com>
2025-03-07 03:46:37 +0530

    fix bug errors524

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,9 @@
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// Add this to ensure the file is recognized as a module
+export {};
