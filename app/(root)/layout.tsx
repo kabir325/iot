d368460f60ba5e32297f7b99469cc3ab5e2a087f
kabir325 <kabirsahu725@gmail.com>
2025-05-30 00:34:11 +0530

    5th commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/navbar"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return ( 
         <main className="min-h-screen bg-background text-foreground font-fragment-mono antialiased">
+            <Analytics />
             <Navbar />
             {children}
         </main>
